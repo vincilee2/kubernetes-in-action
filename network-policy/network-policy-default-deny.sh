fbf20e59b1ddd75db20806e613411d0d2b361fcf
vincilee <vincilee@microsoft.com>
2022-06-04 11:02:18 +0800

    update network-policy-defualt-deny

--- network-policy/network-policy-default-deny.sh
+++ network-policy/network-policy-default-deny.sh
@@ -1,6 +1,8 @@
+# create namespace
+kubectl create namespace n2
 # create pod
 kubectl run nginx1 --image=nginx --restart=Never --labels=app=v1
-kubectl run nginx2 --image=nginx --restart=Never --labels=app=v2
+kubectl run nginx2 --image=nginx --restart=Never --labels=app=v2 -n n2
 
 sleep 10
 # get nginx1 pod ip
@@ -8,7 +10,7 @@ ip1=$(kubectl get pod nginx1 -o jsonpath='{.status.podIP}')
 
 # try connect ngix1 from ngix2
 echo "try curl $ip1 from nginx2"
-kubectl exec -it nginx2 -- env ip1=$ip1 bash -c 'echo curl ${ip1} from nginx2; curl ${ip1}'
+kubectl exec -it nginx2 -n -- env ip1=$ip1 bash -c 'echo curl ${ip1} from nginx2; curl ${ip1}'
 
 # create network-policy
 kubectl create -f network-policy-default-deny.yaml
@@ -16,9 +18,10 @@ kubectl create -f network-policy-default-deny.yaml
 sleep 10
 # try connect ngix1 from ngix2 again
 echo "try curl $ip1 from nginx2 after creating deny network policy"
-kubectl exec -it nginx2 -- env ip1=$ip1 bash -c 'echo curl ${ip1} from nginx2 after creating deny netwoek plicy; curl ${ip1}'
+kubectl exec -it nginx2 -n n2 -- env ip1=$ip1 bash -c 'echo curl ${ip1} from nginx2 after creating deny netwoek plicy; curl ${ip1}'
 
 # delete resources
 kubectl delete pod nginx1
 kubectl delete pod nginx2
 kubectl delete networkpolicy default-deny 
+kubectl delete namespace n2
