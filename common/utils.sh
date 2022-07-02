df0c68d5035fc23db72f985f0566ddb58773f730
vincilee <vincilee@microsoft.com>
2022-07-02 15:11:25 +0800

    access via service

--- common/utils.sh
+++ common/utils.sh
@@ -10,3 +10,18 @@ waitForPodCreated ()
         sleep 1
     done
 }
+
+getPodIp()
+{
+    pod=$1
+    namespace=$2
+    ip=$(kubectl get pod $pod -n $namespace -o jsonpath='{.status.podIP}')
+    echo $ip
+}
+
+getSvcQueryHost()
+{
+    svcName=$1
+    namespace=$2
+    echo "$svcName.$namespace.svc.cluster.local"
+}
--- network-policy/network-policy-deny-other-namespaces.sh
+++ network-policy/network-policy-deny-other-namespaces.sh
@@ -2,29 +2,27 @@ set -a
 . "../common/utils.sh"
 
 # create namespace
+kubectl create namespace n1
 kubectl create namespace n2
 # create pod
-kubectl run nginx1 --image=nginx --restart=Never --labels=app=v1
-kubectl run nginx2 --image=nginx --restart=Never --labels=app=v2 -n n2
+kubectl run nginx1 --image=nginx --restart=Never --labels=app=v1 -n n1 --expose --port=80
+kubectl run nginx2 --image=nginx --restart=Never --labels=app=v2 -n n2 --expose --port=80
 
-waitForPodCreated nginx1 default
+waitForPodCreated nginx1 n1
 waitForPodCreated nginx2 n2
-# get nginx1 pod ip
-ip1=$(kubectl get pod nginx1 -o jsonpath='{.status.podIP}')
-
 # try connect ngix1 from ngix2
-echo "try curl $ip1 from nginx2"
-kubectl exec -it nginx2 -n n2 -- env ip1=$ip1 bash -c 'echo curl ${ip1} from nginx2; curl ${ip1}'
+n1SvcHost=$(getSvcQueryHost nginx1 n1)
+
+echo "try curl $n1SvcHost from nginx2"
+kubectl exec -it nginx2 -n n2 -- env hostAddress=$n1SvcHost bash -c 'echo curl ${hostAddress} from nginx2; curl ${hostAddress}'
 
 # create network-policy
-kubectl create -f network-policy-deny-other-namespaces.yaml
+kubectl create -f network-policy-deny-other-namespaces.yaml -n n1
 
 # try connect ngix1 from ngix2 again
-echo "try curl $ip1 from nginx2 after creating deny network policy"
-kubectl exec -it nginx2 -n n2 -- env ip1=$ip1 bash -c 'echo curl ${ip1} from nginx2 after creating deny netwoek plicy; curl ${ip1}'
+echo "try curl $n1SvcHost from nginx2 after creating deny network policy"
+kubectl exec -it nginx2 -n n2 -- env hostAddress=$n1SvcHost bash -c 'echo curl ${hostAddress} from nginx2; curl ${hostAddress}'
 
 # delete resources
-kubectl delete pod nginx1
-kubectl delete pod nginx2 -n n2
-kubectl delete networkpolicy deny-other-namespaces 
+kubectl delete namespace n1
 kubectl delete namespace n2
